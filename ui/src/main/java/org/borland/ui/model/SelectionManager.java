7453b268f9f4d16ae97d6deb60f204a0593a6fb6
Borland04 <McGarret160497@gmail.com>
2020-05-14 21:33:29 +0300

    Add logging

--- ui/src/main/java/org/borland/ui/model/SelectionManager.java
+++ ui/src/main/java/org/borland/ui/model/SelectionManager.java
@@ -4,29 +4,37 @@ import com.sun.istack.internal.NotNull;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.borland.ui.util.ArrayCompareUtils;
 
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
-// TODO: logging
 // TODO: javadoc
 public class SelectionManager extends Observable<SelectionManager> {
+    private static Logger logger = LogManager.getLogger(SelectionManager.class);
+
     private final List<Observer<? super SelectionManager>> observers = new LinkedList<>();
     private final List<String> selectedObjects = new LinkedList<>();
 
     public void clearSelection() {
+        logger.debug("Clear selection");
         selectedObjects.clear();
         onChanged();
     }
 
     // TODO: javadoc -- if new list is the same, event won't be triggered
     public void setSelectedObjects(List<String> selectedObjects) {
+        logger.debug("Old selection: {}", this.selectedObjects);
+        logger.debug("New selection: {}", selectedObjects);
+
         boolean doesNewSelectionTheSame =
                 ArrayCompareUtils.areUnorderedListsEqual(selectedObjects, this.selectedObjects);
         if(doesNewSelectionTheSame) {
-            // TODO: logging
+            // There's nothing we should do
+            logger.debug("New selection is the same as before. Skip any work and do not notify observers");
             return;
         }
 
@@ -37,32 +45,42 @@ public class SelectionManager extends Observable<SelectionManager> {
     public void setSelectedObjectsInternal(List<String> selectedObjects) {
         this.clearSelection();
         this.selectedObjects.addAll(selectedObjects);
+        logger.debug("Selection was changed. New selection: {}", this.selectedObjects);
+
         onChanged();
     }
 
     public void addSelection(@NotNull String selection) {
+        logger.debug("Select item '{}'", selection);
         if(isSelected(selection)) {
             // There's nothing we should do
-            // TODO: logging
+            logger.debug("Item '{}' was selected already. " +
+                    "Skip any work and do not notify observers", selection);
             return;
         }
 
         selectedObjects.add(selection);
+        logger.debug("Item '{}' was added to a selection list", selection);
         onChanged();
     }
 
     public void removeSelection(@NotNull String selection) {
+        logger.debug("Unselect item '{}'", selection);
         if(!isSelected(selection)) {
             // There's nothing we should do
-            // TODO: logging
+            logger.debug("Item '{}' wasn't selected. " +
+                    "Skip any work and do not notify observers", selection);
             return;
         }
 
         selectedObjects.removeIf(id -> selection.equals(id));
+        logger.debug("Item '{}' was removed from a selection list", selection);
         onChanged();
     }
 
     public void toggleSelection(@NotNull String selection) {
+        logger.debug("Toggle selection of item '{}'", selection);
+        logger.debug("Is item selected: {}", isSelected(selection));
         if(isSelected(selection)) {
             removeSelection(selection);
         }
@@ -72,23 +90,27 @@ public class SelectionManager extends Observable<SelectionManager> {
     }
 
     public boolean isSelected(@NotNull String id) {
-        return selectedObjects.stream()
-            .anyMatch(selectedId -> id.equals(selectedId));
+        boolean isSelected = selectedObjects.stream()
+                .anyMatch(selectedId -> id.equals(selectedId));
+        logger.trace("Is item '{}' selected: {}", id, isSelected);
+        return isSelected;
     }
 
     public List<String> getSelectedObjects() {
         LinkedList<String> copy = new LinkedList<>();
         Collections.copy(copy, selectedObjects);
+        logger.debug("Selected objects: {}", copy);
         return copy;
     }
 
     @Override
     protected void subscribeActual(@NonNull Observer<? super SelectionManager> observer) {
+        logger.debug("Subscribe on SelectionManager");
         observers.add(observer);
     }
 
     private void onChanged() {
-        // TODO: logging
+        logger.debug("SelectionManager was changed. Notify all observers");
         observers.stream()
                 .forEach(observer -> observer.onNext(this));
     }
--- ui/src/main/java/org/borland/ui/util/ArrayCompareUtils.java
+++ ui/src/main/java/org/borland/ui/util/ArrayCompareUtils.java
@@ -1,17 +1,37 @@
 package org.borland.ui.util;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class ArrayCompareUtils {
 
+    private static Logger logger = LogManager.getLogger(ArrayCompareUtils.class);
+
     // TODO: unit-tests
+    /**
+     * Compare two lists. They are equal, if both of them includes the same elements in the same amount.
+     * Order is not important
+     * @param listA
+     * @param listB
+     * @param <T> type of items in list
+     * @return true if lists are equal(Omitting order). False otherwise
+     */
     public static <T> boolean areUnorderedListsEqual(List<T> listA, List<T> listB) {
+        logger.trace("Compare list {} with {}", listA, listB);
+
         Map<T, Integer> freqsA = getItemsFrequencies(listA);
         Map<T, Integer> freqsB = getItemsFrequencies(listB);
 
-        return areItemsFrequenciesEqual(freqsA, freqsB);
+        logger.debug("listA frequencies map: {}", freqsA);
+        logger.debug("listB frequencies map: {}", freqsB);
+
+        boolean areListsEqual = areItemsFrequenciesEqual(freqsA, freqsB);
+        logger.debug("Are lists equal: {}", areListsEqual);
+        return areListsEqual;
     }
 
     private static <T> Map<T, Integer> getItemsFrequencies(List<T> items) {
@@ -22,10 +42,12 @@ public class ArrayCompareUtils {
             result.put(item, prevFreq + 1);
         }
 
+        logger.trace("List {}, frequencies map: {}", items, result);
         return result;
     }
 
     private static <T> boolean areItemsFrequenciesEqual(Map<T, Integer> freqsA, Map<T, Integer> freqsB) {
+        logger.trace("Compare frequencies {} with frequencies {}", freqsA, freqsB);
         boolean doesBContainsAllItems = freqsA.entrySet().stream()
                 .map(entry -> {
                     T key = entry.getKey();
@@ -34,8 +56,10 @@ public class ArrayCompareUtils {
                 })
                 .reduce(true, (acc, val) -> acc && val);
 
-        return freqsA.entrySet().size() == freqsB.entrySet().size()
+        boolean areFreqsEqual = freqsA.entrySet().size() == freqsB.entrySet().size()
                 && doesBContainsAllItems;
+        logger.trace("Are frequencies equal: {}", areFreqsEqual);
+        return areFreqsEqual;
     }
 
 }
